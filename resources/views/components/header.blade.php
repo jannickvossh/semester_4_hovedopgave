7e84117e06859e2cf4cfc1595d2fb4217ea1ca78
minihauner <jannickhaunstrup@gmail.com>
2025-10-21 14:21:26 +0200

    add main.css and main.js

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ isset($title) ? "$title | Hovedopgave på 4. semester" : 'Hovedopgave på 4. semester' }}</title>
+    <link rel="stylesheet" href="{{ URL::asset('/css/main.css') }}">
+    <script src="{{ URL::asset('/js/main.js') }}" defer></script>
     @yield('head')
 </head>
 <body>
